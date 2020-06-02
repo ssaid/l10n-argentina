4105d22b5ecda96e38abdfeed193c093da0f21de
Felipe Rafael Carlini <felipercarlini@gmail.com>
2020-06-02 19:30:12 -0300

    [MOD] context past as reference in to_draft and to_wallet methods

--- l10n_ar_account_check/account_check.py
+++ l10n_ar_account_check/account_check.py
@@ -88,11 +88,11 @@ class account_issued_check(osv.osv):
     }
 
     def to_wallet(self, cr, uid, ids, context=None):
-        self.write(cr, uid, ids, {'state': 'wallet'}, context)
+        self.write(cr, uid, ids, {'state': 'wallet'}, context=None)
         return True
 
     def to_draft(self, cr, uid, ids, context=None):
-        self.write(cr, uid, ids, {'state': 'draft'}, context)
+        self.write(cr, uid, ids, {'state': 'draft'}, context=None)
         return True
 
     def create_voucher_move_line(self, cr, uid, check, voucher, context={}):
